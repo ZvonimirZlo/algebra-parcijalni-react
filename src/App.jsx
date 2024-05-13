1b4a84a6bca6dc021c0c39d0c848b8a5040c2e8b
ZvonimirZlo <lovrez2@gmail.com>
2024-05-13 23:08:01 +0200

    handled users search

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,33 @@
-import './App.css'
-import Search from 'Search/Search';
+import { useState } from 'react';
+import './App.css';
+import Search from './Search/Search';
+import Users from './Users/Users';
 
 function App() {
+  const [data,setData]=useState({});
+  const [isLoading,setIsLoading]=useState(true);
 
   return (
-    <>
-<h1>Hello someone!</h1>
-    </>
+    <div>
+     <div>
+      <Search setData={setData} setIsLoading={setIsLoading} />
+     </div>
+     <div>
+      <Users 
+       avatarUrl={data.avatar_url}
+       email={data.email}
+       followers={data.followers}
+       following={data.following}
+       location={data.location}
+       login={data.login}
+       name={data.name}
+       public_repos={data.public_repos}
+       isLoading={isLoading}
+       bio={data.bio}
+       joinedAt={data.created_at?.split("T")[0]}
+      />
+     </div>
+    </div>
   )
 }
 
